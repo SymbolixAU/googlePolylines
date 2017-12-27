817daf47f51392ddc030dfd810a470d87a43eb9a
tospig <david.cooley141@gmail.com>
2017-12-27 09:52:42 +0000

    wkt separators

--- src/googleWKT.cpp
+++ src/googleWKT.cpp
@@ -22,9 +22,89 @@ void addLonLatToWKTStream(std::ostringstream& os, Rcpp::String lon, Rcpp::String
   
 }
 
-void beginWKT(std::ostringstream& os, Rcpp::String geom) {
-  std::string sgeom = geom;
-  os << sgeom << "(";
+void geom_type(const char *cls, int *tp = NULL) {
+  
+  int type = 0;
+  if (strcmp(cls, "POINT") == 0)
+    type = POINT;
+  else if (strcmp(cls, "MULTIPOINT") == 0)
+    type = MULTIPOINT;
+  else if (strcmp(cls, "LINESTRING") == 0)
+    type = LINESTRING;
+  else if (strcmp(cls, "POLYGON") == 0)
+    type = POLYGON;
+  else if (strcmp(cls, "MULTILINESTRING") == 0)
+    type = MULTILINESTRING;
+  else if (strcmp(cls, "MULTIPOLYGON") == 0)
+    type = MULTIPOLYGON;
+  else
+    type = UNKNOWN;
+  if (tp != NULL)
+    *tp = type;
+}
+
+void beginWKT(std::ostringstream& os, Rcpp::CharacterVector cls) {
+  
+  int tp;
+  geom_type(cls[1], &tp);
+  
+  switch( tp ) {
+  case POINT:
+    os << "POINT(";
+    break;
+  case MULTIPOINT:
+    os << "MULTIPOINT((";
+    break;
+  case LINESTRING:
+    os << "LINESTRING(";
+    break;
+  case MULTILINESTRING:
+    os << "MULTILINESTRING((";
+    break;
+  case POLYGON:
+    os << "POLYGON((";
+    break;
+  case MULTIPOLYGON:
+    os << "MULTIPOLYGON(((";
+    break;
+  default: {
+      Rcpp::stop("Unknown geometry type");
+    }
+  }
+}
+
+void endWKT(std::ostringstream& os, Rcpp::CharacterVector cls) {
+  
+  int tp;
+  geom_type(cls[1], &tp);
+  
+  switch( tp ) {
+  case POINT:
+    os << ")";
+    break;
+  case MULTIPOINT:
+    os << "))";
+    break;
+  case LINESTRING:
+    os << ")";
+    break;
+  case MULTILINESTRING:
+    os << "))";
+    break;
+  case POLYGON:
+    os << "))";
+    break;
+  case MULTIPOLYGON:
+    os << ")))";
+    break;
+  default: {
+      Rcpp::stop("Unknown geometry type");
+    }
+  }
+}
+
+void coordSeparateWKT(std::ostringstream& os) {
+  os << ", ";
 }
 
 // TODO:
@@ -32,25 +112,41 @@ void beginWKT(std::ostringstream& os, Rcpp::String geom) {
 // - if not supplied, assume LINESTRING / MULTILINESTRING 
 // - if from sfencoded - read the attributes
 
-// 
+// - different beginnings based on the geometry -> POLYGON((, LINESTRING(, POINT(, MULTIPOINT((, MULTILINESTRING((, 
+// - SPLIT_CHAR -> the beginning of a new polygon (in a MULTIPOLYGON)
 
 // [[Rcpp::export]]
 Rcpp::String polyline_to_wkt(Rcpp::List sfencoded) {
   
   Rcpp::Rcout << sfencoded.size() << std::endl;
   std::ostringstream os;
+  Rcpp::String wkt;
   
   Rcpp::StringVector pl = sfencoded[0];
   Rcpp::CharacterVector cls = pl.attr("sfc");
-  beginWKT(os, cls[1]);
-
-  Rcpp::String wkt;
   
-  for(size_t i = 0; i < pl.size(); i ++ ) {
+  beginWKT(os, cls);
+  int n =  pl.size();
+
+  for(size_t i = 0; i < n; i ++ ) {
+    
+    // for each set of coordiantes, they need to be split by "),(",
+    // unless it's a new polygon, where it needs ")), ((" 
+    // which is denoted by the SPLIT_CHAR
     Rcpp::String spl = pl[i];
-    std::string stdspl = spl;
-    polylineToWKT(os, stdspl);
+    
+    if(spl == SPLIT_CHAR){
+      os << ")),((";
+    }else{
+      std::string stdspl = spl;
+      polylineToWKT(os, stdspl);
+      if (i < (n-1)){
+        os << "),(";
+      }
+    }
+    
   }
+  endWKT(os, cls);
   
   wkt = os.str();
   return wkt;
@@ -64,9 +160,6 @@ void polylineToWKT(std::ostringstream& os, std::string encoded){
   int index = 0;
   float lat = 0;
   float lng = 0;
-  
-//  std::ostringstream os;
-  Rcpp::String wkt;
   
   while (index < len){
     char b;
@@ -91,6 +184,11 @@ void polylineToWKT(std::ostringstream& os, std::string encoded){
     lng += dlng;
     
     addLonLatToWKTStream(os, lng, lat);
+    if(index < len) {
+      coordSeparateWKT(os);
+//    }else{
+//      endWKT(os, cls);
+    }
   }
 }
 
--- src/googleWKT.h
+++ src/googleWKT.h
@@ -4,6 +4,13 @@ using namespace Rcpp;
 #ifndef GOOGLEWKT_H
 #define GOOGLEWKT_H
 
+#define UNKNOWN           0
+#define POINT             1
+#define MULTIPOINT        2
+#define LINESTRING        3
+#define MULTILINESTRING   4
+#define POLYGON           5
+#define MULTIPOLYGON      6
 
 
 
