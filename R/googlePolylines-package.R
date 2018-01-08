049a9da2d2ca6fe514a2a59ecf1624c11c45bb3e
David Cooley <dcooley@symbolix.com.au>
2018-01-08 16:06:05 +1100

    subset drop

--- R/googlePolylines-package.R
+++ R/googlePolylines-package.R
@@ -12,13 +12,49 @@ str.encoded_column <- function(object, ...) {
   }
 }
 
+## TODO:
+## - having drop = FALSE will override the default `[.data.frame` method
+## is this what I want?
+## - if drop = TRUE, and a single column is selected (the encoded_column), 
+## return the list, give it an 'sfencoded' class, and make it usable in googleway
+## - should I remove 'sfencoded' class if the geometry is dropped? 
+## because they could re-attach it....
+
 
 #' @export
-`[.sfencoded` <- function(x, i, j, drop = FALSE) { 
+`[.sfencoded` <- function(x, i, j, drop = TRUE) { 
   
   geomColumn <- attr(x, "encoded_column")
+  wktColumn <- attr(x, "wkt_column")
+#  sfAtts <- attr(x, "sfAttributes")
+  
   x <- `[.data.frame`(x, i, j, drop)
-  if( geomColumn %in% names(x) ) attr(x, "encoded_column") <- geomColumn
+  
+  if( !is.null(geomColumn) ) { 
+    if( geomColumn %in% names(x) ) {
+      attr(x, "encoded_column") <- geomColumn
+    }
+  }
+  
+  if( !is.null(wktColumn) ) {  
+    if( wktColumn %in% names(x) ) {
+      attr(x, "wkt_column") <- wktColumn
+    }
+  }
+  
+  
+  ## sfAttributes relates to the whole data.frame, so attaching it 
+  ## back on doesn't make sense
+  # if( !is.null(sfAtts) ) {
+  #   attr(x, "sfAttributes") <- sfAtts
+  # }
+  # 
+  # if( inherits(x, "list") ) 
+  #   class(x) <- c("sfencoded_list", class(x))
+  
+  # if ( is.null(geomColumn) && is.null(wktColumn) ) 
+  #   class(x) <- setdiff(class(x),"sfencoded")
+   
   return(x)
 }
 
