ebfa247ca0ac5f6956f3bf7b8f8004ddee513a4e
David Cooley <dcooley@symbolix.com.au>
2018-01-23 15:52:52 +1100

    tidying

--- R/sfencoded.R
+++ R/sfencoded.R
@@ -15,14 +15,6 @@ str.encoded_column <- strSfEncoded
 str.wkt_column <- strSfEncoded
 
 
-## TODO:
-## - if drop = TRUE, and a single column is selected (the encoded_column),
-## return the list, give it an 'sfencoded' class, and make it usable in googleway
-## - should I remove 'sfencoded' class if the geometry is dropped?
-## because they could re-attach it....
-## - include sfAttributes in polyline_wkt
-
-
 #' @export
 `[.sfencoded` <- function(x, i, j, ..., drop = TRUE) {
 
@@ -122,41 +114,4 @@ printSfEncodedPrefix <- function(e, encType) {
   return(e)
   
 }
-
-# nc <- sf::st_read(system.file("shape/nc.shp", package="sf"))
-# enc <- encode(nc)
-# wkt <- polyline_wkt(enc)
-# df <- enc
-# df$wkt <- wkt$geometry
-
-# nc2 <- rbind(nc, nc, nc, nc, nc, nc, nc, nc, nc, nc, nc, nc, nc, nc, nc, nc, nc, nc, nc, nc)
-# nc3 <- rbind(nc2, nc2, nc2, nc2, nc2, nc2, nc2, nc2, nc2, nc2, nc2, nc2)
-
-
-# library(sf)
-# df <- data.frame(myId = c(1,1,1,1,1,1,1,1,2,2,2,2),
-# 								 lineId = c(1,1,1,1,2,2,2,2,1,1,1,2),
-# 								 lon = c(-80.190, -66.118, -64.757, -80.190,  -70.579, -67.514, -66.668, -70.579, -70, -49, -51, -70),
-# 								 lat = c(26.774, 18.466, 32.321, 26.774, 28.745, 29.570, 27.339, 28.745, 22, 23, 22, 22))
-#
-# p1 <- as.matrix(df[4:1, c("lon", "lat")])
-# p2 <- as.matrix(df[8:5, c("lon", "lat")])
-# p3 <- as.matrix(df[9:12, c("lon", "lat")])
-#
-# point <- sf::st_sfc(sf::st_point(x = c(df[1,"lon"], df[1,"lat"])))
-# multipoint <- sf::st_sfc(sf::st_multipoint(x = as.matrix(df[1:2, c("lon", "lat")])))
-# polygon <- sf::st_sfc(sf::st_polygon(x = list(p1, p2)))
-# linestring <- sf::st_sfc(sf::st_linestring(p3))
-# multilinestring <- sf::st_sfc(sf::st_multilinestring(list(p1, p2)))
-# multipolygon <- sf::st_sfc(sf::st_multipolygon(x = list(list(p1, p2), list(p3))))
-# #
-# sf <- rbind(
-# 	st_sf(geometry = polygon),
-# 	st_sf(geometry = multipolygon),
-# 	st_sf(geometry = multilinestring),
-# 	st_sf(geometry = linestring),
-# 	st_sf(geometry = point),
-#   st_sf(geometry = multipoint)
-# 	)
-# enc <- encode(sf)
 
