afa8f8d61edc10401dc7d57c1a4cd9019c2dea8a
SymbolixAU <dcooley@symbolix.com.au>
2018-09-28 06:48:19 +1000

    merged #35 and updated description

--- R/scratch.R
+++ R/scratch.R
@@ -1,4 +1,20 @@
 
+# library(sf)
+# 
+# pl1 <- c(0,0,1,0,1,1,0,1,0,0)  ## start and end elevation must match
+# pl1 <- sf::st_polygon(x = list(matrix(pl1, ncol = 2, byrow = T)))
+# pl2 <- c(0,0,-1,0,-1,-1,0,-1,0,0)
+# pl2 <- sf::st_polygon(x = list(matrix(pl2, ncol = 2, byrow = T)))
+# 
+# ## MULTIPOLYGON
+# sf <- sf::st_sf(geometry = sf::st_sfc(sf::st_multipolygon(x = list(pl1, pl2))))
+# 
+# enc <- encode( sf )
+# 
+# enc$geometry[[1]]
+
+
+
 
 
 
